c65e1a8ec4697784ae0e8a846e6b99e8a7587a6a
Farid Kerimov <45530827+kfarid@users.noreply.github.com>
2019-03-03 00:43:25 +0400

    material

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+        this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { FormControl } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    FormControl
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
